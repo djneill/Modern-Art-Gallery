926f4ba8d0d6959f9500bd6aa55b55f9b45643a1
DJ Neill <dj.neill856@gmail.com>
2024-03-24 22:06:14 -0500

    fix body grid alignment

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -16,9 +16,9 @@ const outfit = Outfit({
 
 export default function Body() {
     return (
-        <main className='hidden md:block px-20 mx-20 my-20 '>
+        <main className='hidden md:block px-20 mx-20 my-20 overflow-hidden'>
             {/* md:w-[1110px] md:h-[1150px] md:inset-x-[165px] md:inset-y-[980px] */}
-            <div className='flex my-8 '>
+            <div className='flex my-8'>
                 <div className='w-2/5'>
                     <h2 className={`${bigShouldersDisplay.className} w-[350px] uppercase almostBlack text-6xl leading-[50px] font-bold my-4`}>{bodyData[1].description}</h2>
                     <h2 className={`${bigShouldersDisplay.className} w-[350px] uppercase almostBlack text-6xl leading-[50px] font-bold`}>{bodyData[2].description}</h2>
@@ -35,8 +35,8 @@ export default function Body() {
             <div className='flex my-8'>
                 <Image
                     src="/assets/desktop/image-grid-2@2x.jpg"
-                    alt="Image of the gallery room."
-                    className='w-3/5 h-[720px] mx-4'
+                    alt="Image of the gallery painting from behind a bench."
+                    className='w-3/5 h-[720px] pr-4'
                     width={635}
                     height={720}
                 />
@@ -44,14 +44,14 @@ export default function Body() {
                 <div className='flex flex-col w-2/5 '>
                     <Image
                         src="/assets/desktop/image-grid-3@2x.jpg"
-                        alt="Image of the gallery room."
-                        className='h-[313px]'
+                        alt="Image of the gallery hallway with painting hanging on the wall."
+                        className='w-full h-[313px]'
                         width={445}
                         height={313}
                     />
-                    <div className='w-[445px] h-[377px] bg-[#141414] mt-8'>
+                    <div className='w-full h-[375px] bg-[#141414] mt-8 overflow-hidden'>
                         <div className='w-[295px] h-[198px] inset-x-[40px] inset-y-[2110px] py-12 pl-8'>
-                            <h1 className={`${bigShouldersDisplay.className} uppercase text-6xl font-bold text-[#FFFFFF] leading-[50px} mx-auto pb-8`}>
+                            <h1 className={`${bigShouldersDisplay.className} uppercase text-6xl font-bold text-[#FFFFFF] leading-[50px} pb-8`}>
                                 Come & Be inspired
                             </h1>
                             <p className={`${outfit.className} w-[340px] text-[18px] leading-[28px] font-normal text-[#FFFFFF]`}>We&apos;re excited to welcome you to our gallery and see how our collections influence you</p>
@@ -59,7 +59,6 @@ export default function Body() {
                     </div>
                 </div>
             </div>
-
         </main>
     )
 }
