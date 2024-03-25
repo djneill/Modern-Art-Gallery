4aac8933178b9c332b378a304d9f58d7c2845bd4
DJ Neill <dj.neill856@gmail.com>
2024-03-24 20:58:10 -0500

    finish staggered grid layout, footer for desktop view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero"
 import Body from "@/components/Body"
 import MobileBody from "@/components/MobileBody";
 import MobileFooter from "@/components/MobileFooter";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Body />
       <MobileBody />
+      <Footer />
       <MobileFooter />
 
     </main>
